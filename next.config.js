a1fb5541ead410688016ebb77bebccdea71f00e7
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2023-12-02 16:06:30 +0200

    Trace the content directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
-    reactStrictMode:true
+    reactStrictMode:true,
+    experimental: {
+      outputFileTracingIncludes: {
+        '/*': ['./content/**/*'],
+      },
+    },
   })
   module.exports = withBundleAnalyzer({})

38b95f1f6987dad91350b3c8b31663737fee8477
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2023-12-03 07:04:25 +0200

    Add Space Mono and Comfortaa fonts

---
+++ public/fonts/fonts.js
@@ -0,0 +1,5 @@
+import { Space_Mono } from "next/font/google";
+import { Comfortaa } from "next/font/google";
+
+export const space_mono = Space_Mono({ subsets: ["latin"], variable: "--ff-mono", weight: ['400', '700'] });
+export const comfortaa = Comfortaa({ subsets: ["latin"], variable: "--ff-base", weight: ['300', '400', '700'] });

8d1d8348beab19e97f0818776c8e59f63a3bcb56
Kareem Amr Younes <official.kyounes@gmail.com>
2023-12-03 22:27:46 +0200

    Create styled html component with variables

---
+++ src/app/Root.js
@@ -0,0 +1,22 @@
+'use client'
+import { CSS_LIGHT_PALETTE } from "@/themes/colors";
+import styled from "styled-components";
+
+const Root = styled.html`
+${CSS_LIGHT_PALETTE}
+
+// `
+
+export default Root
+// return (
+//       <Root lang="en" className={clsx(space_mono.variable, comfortaa.variable)}>
+//         {children}
+//     </Root>
+// )
+// export default function Root({ children }) {
+//   return (
+//   <Root lang="en" className={clsx(space_mono.variable, comfortaa.variable)}>
+//         {children}
+//     </Root>
+//   );
+// }

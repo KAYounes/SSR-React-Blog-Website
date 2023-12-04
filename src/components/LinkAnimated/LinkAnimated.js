0ef194bffcffd6baeb14ac11c2fd34b19fbe4f54
Kareem Amr Younes <official.kyounes@gmail.com>
2023-12-04 18:07:47 +0200

    From home

--- src/components/LinkAnimated/LinkAnimated.js
+++ src/components/LinkAnimated/LinkAnimated.js
@@ -8,8 +8,8 @@ import { space_mono } from "$/fonts/fonts";
 
 const variants = {
   tap: { opacity: 1, x: "75%", y: "-95%" },
-  hover: { opacity: 0.85, x: "65%", y: "-85%" },
-  init: { opacity: 0, x: "30%", y: "-50%", rotate: "45deg",  },
+  hover: { opacity: 1, x: "65%", y: "-85%",  }, //filter: 'saturate(100) hue-rotate(74deg) brightness(1.2)'
+  init: { opacity: 1, x: "30%", y: "-50%", rotate: "45deg", filter: 'unset'  },
 }
 
 const variantsArrowHead = {
@@ -32,7 +32,6 @@ function LinkAnimated() {
             whileTap="tap"
             whileHover="hover"
             initial="init"
-            style={{backgroundColor: 'var(--c-red)'}}
             // animate="init"
             >
             Go TO SITE
@@ -42,15 +41,25 @@ function LinkAnimated() {
                 variants={variants}
                 viewBox='0 0 10 15'
                 fill='none'>
-                <path
+                <motion.path
                     d='M5 13L5 7'
-                    stroke-width='2'
-                    stroke-linecap='round'
+                    strokeWidth='2'
+                    strokeLinecap='round'
                     className={styles.arrowTail}
+                    variants={{
+                        hover: { stroke: '#FF0080' },
+                        init: { stroke: 'black' },
+                      }}
                 />
-                <path
+                <motion.path
                     d='M4.58397 1.62404C4.78189 1.32717 5.21811 1.32717 5.41602 1.62404L8.48177 6.22265C8.70329 6.55493 8.46509 7 8.06574 7H5H1.93426C1.53491 7 1.29672 6.55493 1.51823 6.22265L4.58397 1.62404Z'
                     className={styles.arrowHead}
+                    // initial="init"
+                    variants={{
+                        hover: { fill: '#FF0080' },
+                        init: { fill: 'black' },
+                      }
+                      }
                 />
             </motion.svg>
             </motion.span>

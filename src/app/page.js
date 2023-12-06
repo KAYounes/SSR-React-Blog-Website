c9d8fcb898ee017df793e46eb97c74a82d1a4a69
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2023-12-06 16:23:52 +0200

    Fix eslint react key error on blog-preview-card iterator

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,12 @@ export default async function Home() {
     const blogs = await getBlogs("desc");
 
     const blogPreviewCardsDOM = blogs.map(function (blog) {
-        return <BlogPreviewCard blog={blog} />;
+        return (
+            <BlogPreviewCard
+                blog={blog}
+                key={blog.slug}
+            />
+        );
     });
 
     return (
